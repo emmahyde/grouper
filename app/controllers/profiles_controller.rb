748708011980695dea726879362dbbcb40c280a7
Garen Sahagian <g.s.sahagian@gmail.com>
2024-01-21 16:42:02 -0500

    adding an includes to the friendships list on user profile so we can display thefriend's slug

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,13 +11,15 @@ class ProfilesController < ApplicationController
       end
       format.turbo_stream
     end
+
+    User.joins(friendships: [friend: :profile]).first
   end
 
   def friends
     respond_to do |format|
       format.html do
         @user_id = params[:user_id]
-        @friendships = User.find(@user_id).mutual_friendships.page(params[:page]).per(15)
+        @friendships = User.find(@user_id).mutual_friendships.includes(friend: :profile)
 
         render :friends # implicit if removed bc name match, but wanted to be explicit while debugging
       end
