3f1c90a468e66f3fec550761cff0c67cbbb32c05
Stefan Sahagian <g.s.sahagian@gmail.com>
2024-03-22 16:01:37 -0400

    added confirmation for discarding changes to form in modal.

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,12 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  connect(){
-    console.log("Hello STIMULUS!", this.element)
-  }
   static targets = ["modal"]
 
+  connect(){
+    const form = this.modalTarget.querySelector("form")
+    this.initialState = this.serializeForm(form)
+  }
+
   closeModal() {
+    const form = this.modalTarget.querySelector("form")
+    const currentFormState = this.serializeForm(form)
+    if (this.initialState !== currentFormState){
+      const confirmDiscard =  confirm("Are you sure you want to discard your changes?")
+      if (!confirmDiscard){
+        return
+      }
+    }
     this.element.parentElement.removeAttribute("src")
     this.element.remove()
   }
@@ -28,5 +38,9 @@ export default class extends Controller {
     if (e && e.target.id === 'modal-overlay') {
       this.closeModal()
     }
+  }
+
+  serializeForm(formElement){
+    return new URLSearchParams(new FormData(formElement)).toString()
   }
 }
