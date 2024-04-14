37af5975a5f923f6e6b1b9d01725a973c1ec05d6
Emma Hyde <emmajhyde@gmail.com>
2024-04-14 13:15:20 -0400

    chore: rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :require_login, :set_post, only: %i[ show edit update destroy ]
+  before_action :require_login, :set_post, only: %i[show edit update destroy]
 
   # GET /posts/1
   def show
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -47,20 +47,21 @@ class PostsController < ApplicationController
     @post.destroy!
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
     end
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    def post_params
-      params.require(:post).permit(:text, :user_id)
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def require_login
-      redirect_to login_path unless current_user
-    end
+  def post_params
+    params.require(:post).permit(:text, :user_id)
+  end
+
+  def require_login
+    redirect_to login_path unless current_user
+  end
 end
