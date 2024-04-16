a2f2d754f5d957bdbfc67b5474ad57b02e4a7d58
Emma Hyde <emmajhyde@gmail.com>
2024-04-15 20:27:19 -0400

    cleanup application.js file

--- app/client/entrypoints/application.js
+++ app/client/entrypoints/application.js
@@ -1,47 +1,24 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "@assets/stylesheets/application.css"
+// ~ points to app/client, in theory
 import "~/controllers"
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+// Import a stylesheet in app/client/application
+// note, i KNOW this is possible, you can actually do this INSTEAD
+// of using the <%= vite_stylesheet_tag %>, which i discovered when
+// using that made me want to die
+import "@assets/stylesheets/application.css"
 import { Application } from "@hotwired/stimulus"
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from "@hotwired/turbo"
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
-
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
 
 console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
 
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
+// Import all channels. (i tried to use this and globEager wasn't working for me - EH)
 // const channels = import.meta.globEager('./**/*_channel.js')
 
+// these used to be over at javascripts/application.js, basically same thing
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
-// import { AutoSubmitFormController } from "stimulus-library"
-// application.register("auto-submit-form", AutoSubmitFormController)
-
-// this is where the `data-controller` value comes from, i.e. 'dashboard'
-// application.register("friendships", FriendshipsController)
 
 export { application }
 
